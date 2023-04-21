4a298562a0f9f08f78eabb7f02fa7b9398e2edd4
h1l3xx <alksmlkv@gmail.com>
2023-04-21 13:21:24 +0300

    first commit

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,3 +1,20 @@
+import java.io.IOException
+import java.net.DatagramSocket
+import java.net.DatagramPacket
+
 class Server {
-    fun main(){}
+    fun main(){
+        try {
+            val byteArray : ByteArray = ByteArray(5)
+            val ds = DatagramSocket(1050)
+
+            while (true){
+                val pack = DatagramPacket(byteArray, 5)
+                ds.receive(pack)
+                println(pack.data.toString())
+            }
+        }catch (e : IOException){
+            e.printStackTrace()
+        }
+    }
 }
