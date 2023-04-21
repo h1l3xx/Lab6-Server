c818d2b66dece85e568b1e508563a4f49b77e0b0
Artem622 <artemiiko2014@mail.ru>
2023-04-21 15:14:03 +0300

    DatagramChennel

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,20 +1,44 @@
 import java.io.IOException
-import java.net.DatagramSocket
-import java.net.DatagramPacket
+import java.net.InetSocketAddress
+import java.nio.ByteBuffer
+import java.nio.channels.DatagramChannel
 
-class Server {
-    fun main(){
-        try {
-            val byteArray : ByteArray = ByteArray(5)
-            val ds = DatagramSocket(1050)
+object ReceiverProgram {
+    @Throws(IOException::class)
+    fun startReceiver(): DatagramChannel {
+        val receiver = DatagramChannel.open()
+        // Listening SocketAddress
+        val address = InetSocketAddress("172.28.29.31", 1050)
+        receiver.bind(address)
+        println("Receiver started at #$address")
+        return receiver
+    }
 
-            while (true){
-                val pack = DatagramPacket(byteArray, 5)
-                ds.receive(pack)
-                println(pack.data.toString())
-            }
-        }catch (e : IOException){
-            e.printStackTrace()
+    @Throws(IOException::class)
+    fun receiveMessage(receiver: DatagramChannel): String {
+        val buffer = ByteBuffer.allocate(1024)
+        val senderAddress = receiver.receive(buffer)
+        val message = extractMessage(buffer)
+        println("Received message from sender: $senderAddress")
+        return message
+    }
+
+    private fun extractMessage(buffer: ByteBuffer): String {
+        buffer.flip()
+        val bytes = ByteArray(buffer.remaining())
+        buffer[bytes]
+        return String(bytes)
+    }
+
+    @Throws(IOException::class)
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val receiver = startReceiver()
+        while (true) {
+            val message = receiveMessage(receiver)
+            println(" - Message: $message")
         }
+        receiver.close()
+        println("Receiver closed!")
     }
 }
